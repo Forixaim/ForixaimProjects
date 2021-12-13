16e746d16362680813a7c8fc101a4016edf08626
Forixaim <thebest20067@gmail.com>
2021-12-13 12:06:34 -0800

    Added user input test

--- TripleX/Rust/TripleX.rs
+++ TripleX/Rust/TripleX.rs
@@ -1,7 +1,8 @@
 fn main() {
-  let mut Input: u32 = 0;
-    println!("Hello, world!");
-
+    let mut input = String::new();
+    println!("Hello, world!\n");
+    let b1 = std::io::stdin().read_line(&mut input).unwrap();
+    println!("\n{}",input);
+    println!("{}",b1);
+    print!("The great lord Matthew have no knowledge of Rust, go figure it out yourselves");
 }
-
-//Work in Progress
